56676950b8edefd2e13a849e0d13d42afc4eeab3
Jake Conway <jake.h.conway@gmail.com>
2017-03-06 20:36:41 +0000

    Added initial unit tests

---
+++ perspective/tests/test_perspective.py
@@ -0,0 +1,45 @@
+import os
+from perspective import Perspective, allowed
+import random
+import string
+import unittest
+
+api_key = os.environ.get("API_KEY")
+
+def generate_text():
+    paragraph = []
+    sentences = random.randrange(3,6)+1
+    for x in range(sentences):
+        words = random.randrange(2,10)+1
+        sentence = []
+        for y in range(words):
+            characters = random.randrange(0,10)+1
+            word = ""
+            for z in range(characters):
+                word += random.choice(string.ascii_uppercase + string.ascii_lowercase)
+            sentence.append(word)
+        paragraph.append(" ".join(sentence) + ".")
+    return " ".join(paragraph)
+
+class PerspectiveTests(unittest.TestCase):
+    p = Perspective(api_key)
+
+    def test_invalid_test(self):
+        with self.assertRaises(ValueError):
+            self.p.score(generate_text(),tests="NOT_A_TEST")
+
+    def test_invalid_test_type(self):
+        with self.assertRaises(ValueError):
+            self.p.score(generate_text(), texts=0)
+
+    def test_comment(self):
+        for x in range(10):
+            test = random.choice(allowed)
+            comment = self.p.score(generate_text(),tests=test)
+            self.assertTrue(0 <= comment[test].score <= 1)
+            for attr in comment[test].spans:
+                self.assertTrue(0 <= attr.score <= 1)
+                self.assertTrue(str(attr) in comment)
+
+if __name__ == '__main__':
+    unittest.main()
